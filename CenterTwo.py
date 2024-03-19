38d9b6be6cc38991bcb08b07a362c5de1b714b89
federico-cacciotti <fe.cacciotti@gmail.com>
2024-03-19 15:47:07 +0100

    Bug fix

--- CenterTwo.py
+++ CenterTwo.py
@@ -6,6 +6,21 @@ ENQ = b'\x05' # enquiry
 CR =  b'\x0d' # carriage return
 LF =  b'\x0a' # line feed
 
+# error strings
+ACK_ERROR = "Acknowlegment error"
+UNKNOWN_ERROR = "Unknown error"
+INCORRECT_VALUE_ERROR = "Incorrect value"
+
+# sensor status strings
+SENS_OK = "Measurement data ok"
+SENS_UR = "Measurement under range"
+SENS_OV = "Measurement over range"
+TRANS_ERR = "Transmitter error"
+TRANS_OFF = "Transmitter switched off"
+NO_TRANS = "No transmitter"
+ID_ERR = "Identification error"
+ITR_ER = "ITR error"
+
 class Controller():
 
     def __init__(self, serial_port, baudrate=9600, pariy=serial.PARITY_NONE, stopbits=2):
@@ -26,27 +41,68 @@ class Controller():
     def readAcknowledgement(self):
         return self.serial_com.readline().rstrip()
 
+    def checkSensorStatus(self, status):
+        if status == 0:
+            return SENS_OK
+        elif status == 1:
+            return SENS_UR
+        elif status == 2:
+            return SENS_OR
+        elif status == 3:
+            return TRANS_ERR
+        elif status == 4:
+            return TRANS_OFF
+        elif status == 5:
+             return NO_TRANS
+        elif status == 6:
+            return ID_ERR
+        elif status == 7:
+            return ITR_ERR
+        else:
+            return -1
+
     # BAU
-    def setBaudrate(self, mode):
+    def setBaudrate(self, mode=0):
         """
-        mode (int): 0 for 9600, 1 for 19200, or 2 for 38400
+        Baudrate. Transfer rate of the RS232C interface.
+
+        Parameters:
+        mode (int): 0 for 9600 (default), 1 for 19200, or 2 for 38400.
         """
         if mode == 0 or mode == 1 or mode == 2:
             command = ("BAU,{:d}".format(mode)).encode()
         else:
-            print("Incorrect baudrate")
+            print(INCORRECT_VALUE_ERROR)
             return -1
             
         self.sendCommand(command)
-            if self.readAcknowledgement() == ACK:
-                self.enquiry()
-                returned_mode = int(self.readLine().rstrip().decode())
-                if returned_mode == mode:
-                    print("Baudrate succesfully set")
-                    return returned_mode
-                else:
-                    print("Unknown error")
-                    return -1
+        if self.readAcknowledgement() == ACK:
+            self.enquiry()
+            returned_mode = int(self.readLine().rstrip().decode())
+            if returned_mode == mode:
+                print("Baudrate succesfully set")
+                return returned_mode
+            else:
+                print(UNKNOWN_ERROR)
+                return -1
+        else:
+            print(ACK_ERROR)
+            return -1
+    # COM
+    def setContinuousMode(self, period=1):
+        """
+        Continuous mode. Continuous transmission of measurements to the serial interface.
+
+        Parameters:
+        period (int): 0 for 100 milliseconds, 1 for 1 seconds (default), 2 for 1 minute.
+        """
+        command = ("COM,{:d}".format(period)).encode()
+        self.sendCommand(command)
+        if self.readAcknowledgement() == ACK:
+            self.enquiry()
+            s, v = self.readLine().rstrip().decode().split(", ")
+        else:
+            print(ACK_ERROR)
 
     # ERR
     def getErrorStatus(self):
@@ -67,6 +123,9 @@ class Controller():
             if status == '0000':
                 errors.append("No error")
             return status, errors
+        else:
+            print(ACK_ERROR)
+            return -1
 
     # PNR
     def getProgramNumber(self):
@@ -90,25 +149,10 @@ class Controller():
         self.sendCommand(command)
         if self.readAcknowledgement() == ACK:
             self.enquiry()
-            status, value = self.readLine().rstrip().decode().split(", ")
-            status = int(status)
-            value = float(value)
-            
-            if status == 1:
-                print("Measurement under range")
-            elif status == 2:
-                print("Measurement over range")
-            elif status == 3:
-                print("Transmitter error")
-            elif status == 4:
-                print("Transmitter switched off")
-            elif status == 5:
-                print("No transmitter")
-            elif status == 6:
-                print("Identification error")
-            elif status == 7:
-                print("ITR error")
-    
+            s, v = self.readLine().rstrip().decode().split(", ")
+            s = int(s)
+            v = float(v)
+            status = self.checkSensorStatus(s)
             return [status, value]
                 
         else:
